336a38b51145e8590abcf6ddf24ed4b297f05d87
Nikolas Knickrehm <nikolas.knickrehm@isp.uni-luebeck.de>
2020-03-01 20:05:45 +0100

    PM2

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 cd $1
 git stash
 git pull
-pm2 stop your-project
+pm2 stop $2
 npm i
 pm2 start ecosystem.config.js --env production
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('dotenv').config()
 
 const secret = process.env.GITHUB_SECRET;
 const mainRepo = process.env.MAIN_REPO;
+const mainPM2 = process.env.MAIN_PM2;
 
 http.createServer((req, res) => {
   req.on('data', (chunk) => {
@@ -16,7 +17,7 @@ http.createServer((req, res) => {
     console.log(sig);
     if (req.headers['x-hub-signature'] === sig) {
       console.log('Valid request from GitHub');
-      const p = spawn('/bin/bash', ['./deploy.sh', mainRepo]);
+      const p = spawn('/bin/bash', ['./deploy.sh', mainRepo, mainPM2]);
       p.stdout.on('data', (data) => {
         console.log(data.toString());
       });
